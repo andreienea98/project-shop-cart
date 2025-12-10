3be8c1e9f9074be3999562cdf943d201a37da3ed
andreienea398 <andreihd398@gmail.com>
2025-12-10 19:48:37 -0300

    added totalPrice to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,25 @@
+import { formatBRL } from "../utils/formatPrice"
+
 export default function Cart(props) {
+  console.log(props.cart)
+
+  const totalPrice = props.cart.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  )
+
   return (
     <>
       <button
-        onClick={() => props.cart.length > 0 && props.setIsCartOpen(prev => !prev)}
+        onClick={() =>
+          props.cart.length > 0 && props.setIsCartOpen(prev => !prev)
+        }
       >
         Cart {props.cart.length > 0 && props.cart.length}
       </button>
       {props.isCartOpen && (
         <div className="cart-dropdown">
+          <span>Total: {formatBRL(totalPrice)}</span>
           {props.cart.map(item => (
             <div key={item.id} className="cart-item">
               <span>{item.title}</span>
